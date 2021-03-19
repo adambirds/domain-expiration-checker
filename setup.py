11b81828b89ad3d6c8810113d858bb4a66425ee1
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2021-03-19 17:55:29 +0000

    Update setup.py.

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name = "dns-domain-expiration-checker",
     version = "6.0",
-    author = "Matty",
-    author_email = "matty @ prefetch dot net",
-    url = 'https://github.com/Matty9191/dns-domain-expiration-checker',
+    author = "ADB Web Designs",
+    author_email = "adam.birds@adbwebdesigns.co.uk",
+    url = 'https://github.com/adb-web-designs/dns-domain-expiration-checker.git',
     description = "DNS Domain Expiration Checker",
     keywords = "DNS Domain Expiration Bind",
     packages = find_packages(),
     install_requires = ['python-dateutil'],
 )
- 
